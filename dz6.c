0b595a026c290e0a2d276a4c7da6a57080e2e133
gruent <73877605+gruent@users.noreply.github.com>
2025-10-20 23:38:13 +0300

    Add files via upload

---
+++ dz6.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	int N;
+	printf("Введите число N, где N < 1000: ");
+	scanf("%d", &N);
+	if (N >= 1000)
+	{
+		printf("Ошибка, N должно быть меньше 1000\n");
+	}
+	else {
+		int a = N / 100;
+		int b = (N / 10) % 10;
+		int c = N % 10;
+		int  sum = a + b + c;
+		if (sum >= 10)
+		{
+			printf("Сумма цифр числа %d является двухзначным: %d", N, sum);
+		}
+		else
+		{
+			printf("Cумма цифр числа %d является однозначным: %d", N, sum);
+		}
+	}
+
+	}
+	
